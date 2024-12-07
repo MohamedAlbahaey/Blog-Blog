5bca774db2145600fdcc2e893a2a590740150d06
Mohamed Albahaey <144071699+MohamedAlbahaey@users.noreply.github.com>
2024-12-07 13:44:06 +0200

    Intial commit and added Delete

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-var posts = new Map();
-
+var titles = [];
+var texts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,21 +15,39 @@ app.get("/",(req,res) =>{
 });
 
 app.get("/posts",(req,res) =>{
-    res.render("posts.ejs",{map:posts});
+    res.render("posts.ejs",{titles : titles, texts : texts});
+});
+
+app.get("/posts/delete/:blogID",(req,res) =>{
+    var index = req.params.blogID;
+    titles.splice(index,1);
+    texts.splice(index,1);
+
+    res.redirect("../../posts");
+});
+
+app.get("/posts/edit/:blogID",(req,res) =>{
+    var index = req.params.blogID;
+    res.render("create.ejs",{title : titles[index], text : texts[index]});
+});
+
+app.get("/posts/readmore/:blogID",(req,res) =>{
+    var index = req.params.blogID;
+    res.render("readmore.ejs",{title : titles[index], text : texts[index]});
 });
 
 app.get("/create",(req,res) =>{
     res.render("create.ejs");
 });
 
-app.post("/posts",(req,res) => {
-    var title1 = req.body["title"];
-    var text1 = req.body["text"];
-    if(title1){
-        posts.set(title1 , text1)
+app.post("/posts",(req,res) =>{
+    var title = req.body["title"];
+    var text = req.body["text"];
+    if(title){
+        titles.push(title);
+        texts.push(text);
     }
-
-    res.render("posts.ejs",{map : posts});
+    res.render("posts.ejs",{titles : titles, texts : texts});
 });
 
 app.listen(port,() => {
